6912d691eb8cd245e6d33820fc3910157618f444
派大星嘱咐我别忘了学算法 <oncwnuKwbdK_335TDUfKNrdM43Mg@git.weixin.qq.com>
2025-05-23 11:56:01 +0800

    修改了序列化里的文章列表的函数，同时修改了views，让网页里的json数据看起来更美观。

--- article/serializers.py
+++ article/serializers.py
@@ -1,12 +1,24 @@
 from rest_framework import serializers
-
+from .models import Article
 # 序列化：将后端存储的变量转换成json格式；逆序列化：将json转换成前端可以读取的格式
 
 
-# 定义序列化文章列表的方法
-class ArticleListSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(allow_blank=True, max_length=100)
-    body = serializers.CharField(allow_blank=True)
-    created = serializers.DateTimeField()
-    updated = serializers.DateTimeField()
+# 基础定义序列化文章列表的方法
+# class ArticleListSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(allow_blank=True, max_length=100)
+#     body = serializers.CharField(allow_blank=True)
+#     created = serializers.DateTimeField()
+#     updated = serializers.DateTimeField()
+
+# 更常用的方法，父类变成了 ModelSerializer
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'title',
+            'body',
+            'created',
+            'updated',
+        ]
--- article/views.py
+++ article/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Article
 from article.serializers import ArticleListSerializer
 from django.http import JsonResponse
 
+# 较基础的方法
+# def article_list(request):
+#     articles = Article.objects.all()
+#     serializer = ArticleListSerializer(articles, many=True)
+#     return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET', 'POST'])
 def article_list(request):
-    articles = Article.objects.all()
+    # 同一个函数的不同请求方式
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        # 使用定义的序列化方法把文章列表数据序列化一下，将数据库中读取的数据转换成json格式
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data)
 
-    # 使用定义的序列化方法把文章列表数据序列化一下，将数据库中读取的数据转换成json格式
-    serializer = ArticleListSerializer(articles, many=True)
+    elif request.method == 'POST':
+        serializer = ArticleListSerializer(data=request.data)
 
-    return JsonResponse(serializer.data, safe=False)
+        # 数据合法的话就保存了
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # 不合法就反馈错误
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    return None
