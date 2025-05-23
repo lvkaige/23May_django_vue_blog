de594bd73b8ae0a9b8279477c6feca66a5954d72
派大星嘱咐我别忘了学算法 <oncwnuKwbdK_335TDUfKNrdM43Mg@git.weixin.qq.com>
2025-05-23 14:12:16 +0800

    测试了只有管理员可以查看文章列表

--- article/urls.py
+++ article/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.article_list, name='article_list'),
+    # path('', views.article_list, name='article_list'),
+    path('', views.ArticleListView.as_view(), name='article-list'),
     path('<int:pk>/', views.ArticleDetail.as_view(), name='detail'), # 文章详情的url，为什么加上了一个as_view(),是因为views中，ArticleDetail是一个类，并不是函数，as_view是用来处理类class的
 ]
--- article/views.py
+++ article/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from django.http import JsonResponse, Http404
 from article.serializers import ArticleListSerializer, ArticleDetailSerializer
+from rest_framework.permissions import IsAdminUser
 
 from .models import Article
 
@@ -14,40 +15,55 @@ from .models import Article
 #     articles = Article.objects.all()
 #     serializer = ArticleListSerializer(articles, many=True)
 #     return JsonResponse(serializer.data, safe=False)
+#
+# @api_view(['GET', 'POST'])
+# @permission_classes([IsAdminUser]) # 只有管理员可以调用这个函数
+# def article_list(request):
+#     # 同一个函数的不同请求方式
+#     if request.method == 'GET':
+#         articles = Article.objects.all()
+#         # 使用定义的序列化方法把文章列表数据序列化一下，将数据库中读取的数据转换成json格式
+#         serializer = ArticleListSerializer(articles, many=True)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'POST':
+#         serializer = ArticleListSerializer(data=request.data)
+#
+#         # 数据合法的话就保存了
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#
+#         # 不合法就反馈错误
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     return None
 
-@api_view(['GET', 'POST'])
-def article_list(request):
-    # 同一个函数的不同请求方式
-    if request.method == 'GET':
+
+# 这段代码是上方函数的类视图改写
+# noinspection PyMethodMayBeStatic
+class ArticleListView(APIView):
+    permission_classes = [IsAdminUser]  # 仅限管理员访问
+
+    def get(self, request):
         articles = Article.objects.all()
-        # 使用定义的序列化方法把文章列表数据序列化一下，将数据库中读取的数据转换成json格式
         serializer = ArticleListSerializer(articles, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request):
         serializer = ArticleListSerializer(data=request.data)
-
-        # 数据合法的话就保存了
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        # 不合法就反馈错误
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    return None
 
 
 # 用类的方式来写函数
+# noinspection PyMethodMayBeStatic
 class ArticleDetail(APIView):
-    """文章详情视图"""
-
     # 这是一个普通的函数，获取某特定id的article
     def get_object(self, pk):
-        """获取单个文章对象"""
         try:
-            # pk 即主键，默认状态下就是 id
-            # 找到这个article后，将其return回去
             return Article.objects.get(pk=pk)
         except:
             raise Http404
@@ -62,10 +78,8 @@ class ArticleDetail(APIView):
     # 修改article内容
     def put(self, request, pk):
         article = self.get_object(pk)
-
         # 接收到数据，反序列化
         serializer = ArticleDetailSerializer(article, data=request.data)
-
         if serializer.is_valid():
             # 数据合法的话，就保存到数据库
             serializer.save()
@@ -78,3 +92,5 @@ class ArticleDetail(APIView):
         article.delete()
         # 删除成功后返回204
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
